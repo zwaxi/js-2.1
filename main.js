38d85d0d5aca5dfe150e834e84e7cccac42c24b7
zwaxi <muhammadishkuvatov@gmail.com>
2024-05-21 15:07:35 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,28 @@
+let a = prompt('1 dan 7 gacha son kiriting...');
+
+
+switch (a) {
+    case '1':
+        alert('Dushanba');
+        break
+    case '2':
+        alert('Seshanba');
+        break
+    case '3':
+        alert('Chorshanba');
+        break
+    case '4':
+        alert('Payshanba');
+        break
+    case '5':
+        alert('Juma');
+        break
+    case '6':
+        alert('Shanba');
+        break
+    case '7':
+        alert('Yakshanba');
+        break
+
+    default: alert('1 haftada 7 kun bor.')
+}
