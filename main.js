5bc0e0fea339c6ebc4504ac3b81dacb9e0e38437
zwaxi <muhammadishkuvatov@gmail.com>
2024-05-23 15:10:25 +0500

    first commit

--- main.js
+++ main.js
@@ -1,28 +1,10 @@
-let a = prompt('1 dan 7 gacha son kiriting...');
+let a = prompt("Ismingizni kiriting...")
+let b = prompt("Space Id kiriting...")
+let c = prompt("Space password kiriting...")
 
-
-switch (a) {
-    case '1':
-        alert('Dushanba');
-        break
-    case '2':
-        alert('Seshanba');
-        break
-    case '3':
-        alert('Chorshanba');
-        break
-    case '4':
-        alert('Payshanba');
-        break
-    case '5':
-        alert('Juma');
-        break
-    case '6':
-        alert('Shanba');
-        break
-    case '7':
-        alert('Yakshanba');
-        break
-
-    default: alert('1 haftada 7 kun bor.')
+let Space = {
+    name: a,
+    id: b,
+    password: c,
 }
+console.log(Space);
